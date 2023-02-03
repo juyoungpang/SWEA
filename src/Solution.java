b96221253eed281a236a4905343fd316f3bd969f
jenna <jenna990615@gmail.com>
2023-02-03 13:55:01 +0900

    .

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,3 @@
-import java.util.Scanner;
-
-class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-
-		for (int test_case = 1; test_case <= T; test_case++) {
-
-//			System.out.printf("#%d %d%n", test_case, answer);
-		}
-	}
+public class Solution {
+	
 }
