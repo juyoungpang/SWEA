a17e7f39b84ba33b5032bc631d4c77ab606b84ae
juyoungpang <jenna990615@naver.com>
2023-02-07 17:55:13 +0900

    .

--- src/D3/p1208_sort.java
+++ src/D3/p1208_sort.java
@@ -1,14 +1,13 @@
+package D3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution {
-	static int[] boxes;
-	static int dumps;
-	
+public class p1208_sort {
 	public static void main(String[] args) throws IOException{
+		int[] boxes = null;
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -16,7 +15,7 @@ public class Solution {
 		for(int TC=1;TC<=10;TC++) {
 			boxes = new int[100];
 			
-			dumps = Integer.parseInt(br.readLine());
+			int dumps = Integer.parseInt(br.readLine());
 			StringTokenizer tok = new StringTokenizer(br.readLine());
 			for(int i=0;i<100;i++) {
 				boxes[i] = Integer.parseInt(tok.nextToken());
@@ -29,20 +28,9 @@ public class Solution {
 			}
 			
 			Arrays.sort(boxes);
-			sb.append("#").append(TC).append(" ").append(boxes[99]-boxes[0]).append("\n");
+			sb.append("#").append(TC).append(boxes[99]-boxes[0]).append("\n");
 		}
 		
 		System.out.println(sb.toString());
-	}
-	
-	public static void rec(int count) {
-		Arrays.sort(boxes);
-
-		if(count==dumps) {
-			return;
-		}
-		
-		boxes[0]++;
-		boxes[99]--;
 	}
 }
